d290fcb9fd0883d6b0825b0be2ea41f2421a172d
webshining <webshining@protonmail.com>
2025-07-11 16:24:28 +0300

    updated

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,18 +1,10 @@
 import axios from "axios";
 
-const getCookie = (name: string) => {
-	const cookies = document.cookie.split("; ").find((row) => row.startsWith(`${name}=`));
-	return cookies ? cookies.split("=")[1] : null;
-};
 export const host = axios.create({
 	baseURL: process.env.REACT_APP_API_URL,
 	withCredentials: true,
 });
 host.interceptors.request.use((config) => {
-	const token = getCookie("csrftoken");
-	if (token) {
-		config.headers["X-CSRFToken"] = token;
-		config.headers["ngrok-skip-browser-warning"] = "69420";
-	}
+	config.headers["ngrok-skip-browser-warning"] = "69420";
 	return config;
 });
