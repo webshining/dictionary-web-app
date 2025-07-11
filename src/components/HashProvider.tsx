08094088593438378287156dd7c2f8ebd2c148e9
webshining <webshining@protonmail.com>
2025-07-11 03:23:08 +0300

    updated

--- src/components/HashProvider.tsx
+++ src/components/HashProvider.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
-import { Outlet, useNavigate } from "react-router";
+import { Outlet } from "react-router";
 import { host } from "../api";
 
 const HashProvider = () => {
-	const navigate = useNavigate();
 	const [authorized, setAuthorized] = useState<boolean>(false);
 	useEffect(() => {
 		if (window.location.hash) {
@@ -13,7 +12,6 @@ const HashProvider = () => {
 			host.post("/init", { tgWebAppData }).then(({ data }) => {
 				if (data.message == "Success") {
 					setAuthorized(true);
-					navigate(window.location.pathname + window.location.search, { replace: true });
 				}
 			});
 		} else {
